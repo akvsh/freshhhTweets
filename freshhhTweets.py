4d369279df96444bb73ea58d9790839d0a2642a5
Akash Patel <akash_patel97@hotmail.com>
2016-10-17 12:13:01 -0400

    store already posted id in file and check for duplicate tweets

--- freshhhTweets.py
+++ freshhhTweets.py
@@ -2,9 +2,10 @@ import praw
 import tweepy
 
 #TODO
+#store the tweeted post id into temp_file 
 #Make it run every hour or so and check that you aren't posting duplicates
+# make cron job to make it run every hour
 temp_file = 'tweeted.txt'
-
 
 #tokens and stuff needed for twitter API
 access_token = ''
@@ -15,12 +16,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+
+def check_duplicate_submission(id):
+	id_exists = False
+	file = open(temp_file,'r')
+	for posted_id in file:
+		if posted_id == id:
+			id_exists = True
+			break
+	return id_exists
+
 def tweet_fresh_post(title,link,post_id):
 	is_already_posted = check_duplicate_submission(post_id)
 	if(is_already_posted):
-		break
-	else:
+		pass
+#	else:
+
 		#tweet Here
+		#add post_id to the file
 	#tweet with title and post link to post
 
 #def get_hot_posts():
